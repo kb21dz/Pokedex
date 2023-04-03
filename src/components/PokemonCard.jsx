af65b3b8e7a80c942b44fc98f021e1dec47e2bbd
Kader B <kder000@gmail.com>
2023-04-03 14:33:20 +0200

    2nd component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+    
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt=""
-      />
+      {pokemon.imgSrc ? (<img src = {pokemon.imgSrc}/> ): <p>???</p>}
       <figcaption>bulbasaur</figcaption>
     </figure>
   );
 }
 
 export default PokemonCard;
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
