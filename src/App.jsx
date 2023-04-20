c53125d0bf7bb0ffb2acb485310c3d38def26b9e
Kader B <kder000@gmail.com>
2023-04-20 11:15:49 +0200

    state1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import PokemonCard from './components/PokemonCard'
+import { useState } from 'react';
+
+import  PokemonCard  from './components/PokemonCard'
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
+  const [ pokemonIndex, setPokemonIndex ] = useState(0)
+
+    const handlePrecedingClick = () => {
+        setPokemonIndex(pokemonIndex - 1)
+    }
+
+    const handleNextClick = () => {
+        setPokemonIndex(pokemonIndex + 1)
+    }
   return (
     <div>
-      <PokemonCard pokemon = {pokemonList[0]} />
-
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {
+        pokemonIndex > 0 && <button onClick={handlePrecedingClick} >Précedent</button>
+      }
+      {
+        pokemonIndex < pokemonList.length - 1 && <button onClick={handleNextClick} >Suivant</button>
+      }
     </div>
   )
 }
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default App;
